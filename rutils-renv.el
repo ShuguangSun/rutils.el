49281dcf92dce169268b4ad56fa157801c8afbbb
Shuguang Sun <shuguang79@qq.com>
2021-06-25 11:31:16 +0800

    initialization
    Signed-off-by: Shuguang Sun <shuguang79@qq.com>

---
+++ rutils-renv.el
@@ -0,0 +1,35 @@
+;;; rutils-renv.el --- renv with transient           -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shuguang Sun
+
+;; Author: Shuguang Sun <shuguang79@qq.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'ess-inf)
+(require 'ess-r-mode)
+(require 'ess-r-completion)
+(require 'transient)
+
+
+
+(provide 'rutils-renv)
+;;; rutils-renv.el ends here
